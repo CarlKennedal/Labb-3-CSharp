d5a87bfce5bf3fa629ac107e778558973281f059
CarlKennedal <carl.kennedal@gmail.com>
2024-11-06 19:29:55 +0100

    Binding saknas

--- Labb-3-CSharp/ViewModel/ConfigurationViewModel.cs
+++ Labb-3-CSharp/ViewModel/ConfigurationViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Labb_3_CSharp.Command;
+using Labb_3_CSharp.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Labb_3_CSharp.ViewModel
 {
@@ -10,21 +13,29 @@ namespace Labb_3_CSharp.ViewModel
     {
         private readonly MainWindomViewModel? mainWindomViewModel;
 
+        public DelegateCommand AddButtonCommand { get; }
+        public DelegateCommand RemoveButtonCommand { get; }
+
         public ConfigurationViewModel(MainWindomViewModel? mainWindomViewModel)
         {
             this.mainWindomViewModel = mainWindomViewModel;
+
+            AddButtonCommand = new DelegateCommand(AddButton);
+            RemoveButtonCommand = new DelegateCommand(RemoveButton, RemoveButtonActive);
         }
 
-        public QuestionPackViewModel? ActivePack { get => mainWindomViewModel.ActivePack; }
+        public QuestionPackViewModel? ActivePack { get => mainWindomViewModel?.ActivePack; }
         
-        private void AddButton()
+        private void AddButton(object parameter)
         {
-
+           ActivePack.Questions.Add(new Question("Fråga","Rätt svar","Fel svar 1","Fel svar två","Fel svar tre"));
+           AddButtonCommand.RaiseCanExecuteChanged();
         }
-        private void RemoveButton() 
+        private void RemoveButton(object parameter) 
         {
-
+            
         }
-
+        private bool RemoveButtonActive(object? arg)
+        { throw new NotImplementedException(); }
     }
 }
